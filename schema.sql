4b7e753647ce4f1065525e11b4dc54f49a0d4684
Briton Otieno <ojwangbriton@gmail.com>
2023-08-31 08:28:54 +0300

    Add schema file

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
+\c vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL(5, 2)
 );
