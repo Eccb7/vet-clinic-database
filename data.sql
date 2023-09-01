d401f9573a2ac27e69707c40a7c4db143ec93310
Briton Otieno <ojwangbriton@gmail.com>
2023-09-01 10:03:59 +0300

    Add more data

--- data.sql
+++ data.sql
@@ -4,3 +4,13 @@ VALUES
   ('Gabumon', '2018-11-15', 2, true, 8),
   ('Pikachu', '2021-01-07', 1, false, 15.04),
   ('Devimon', '2017-05-12', 5, true, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES
+  ('Charmander', '2020-02-08', 0, false, -11, 'pokemon'),
+  ('Plantmon', '2021-11-15', 2, true, -5.7, 'unspecified'),
+  ('Squirtle', '1993-04-02', 3, false, -12.13, 'pokemon'),
+  ('Angemon', '2005-06-12', 1, true, -45, 'unspecified'),
+  ('Boarmon', '2005-06-07', 7, true, 20.4, 'unspecified'),
+  ('Blossom', '1998-10-13', 3, true, 17, 'unspecified'),
+  ('Ditto', '2022-05-14', 4, true, 22, 'unspecified');
